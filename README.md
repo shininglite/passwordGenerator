091b49e7cdca4b9dace3314e0dcffa5a5d5f17be
shininglite <vandeusent@gmail.com>
2020-03-07 19:35:21 -0600

    message

--- README.md
+++ README.md
@@ -2,20 +2,10 @@
 This project is designed to generate a random password of a length between 8 and 128 characters, based on at least one type of character of four types presented: lowercase, uppercase, numbers, and special symbols.
 
 ## Results
-Results of the program can be viewed in the console after responding to the prompts presented.
-
-## Progress
-I am pleased that I was able to:
-- write pseudo code (below) that sequences a solution to the problem
-- create prompts and assign responses to those prompts to create variables holding the responses
-- create an array of replies (named included) that I later used to check if the user chose at least one type of character
-- successfully test for password length of 8 to 128
-- create a function, getRandomLower, to use character codes combined with functions I have documented in comments
+Results of the program can be viewed in the console after responding to the prompts presented, and in the main window of the document.
 
 ## Improvements to Be Made
-I need to use the function that gets random lowercase characters. Once successful with lowercase, repeat success with other types using similar code.
-
-I need to figure out how to assemble the results, of the requested length, of the types requested, in a random manner, into a string of characters and display it in the document, or at least display it in the console.
+Styling via an external style sheet, or a framework such as Bootstrap could make the visual end result more appealing or interesting
 
 ## Pseudocode for JavaScript Password Generator
 
@@ -29,25 +19,23 @@ Keep track of which types of characters user wants included; (lower, upper, nume
 
 If user has selected at least one type of character to include in the password, then proceed, otherwise quit.
 
-## Valid Characters by Type
+### Valid Characters by Type
 lower are abcdefghijklmnopqrstuvwxyz
 upper are ABCDEFGHIJKLMNOPQRSTUVWXYZ
 numeric are 0123456789
-special are ~`!@#$%^&*()-_=+[]{}\|;:'",<.>/ and ?.
+special are !@#$%^.
 
 ### Prompt for password length
 User is asked for length of password that is desired. It must at least 8 characters and no longer than 128 characters to be valid.
 
 If user inputs a valid number (an integer greater than 7 and less than 129), continue, otherwise quit. 
 
-### Proof of concept
-Choosing among lowercase letters only, randomly pick characters from that character set to create a password that is the length requested by the user.
-
 ### Program randomly chooses characters among requested types
-Then, choosing only among the desired character (types), the program randomly chooses characters for inclusion in the password, one at a time, until the desired length password is reached.
+Then, choosing only among the desired character (types), the program randomly adds valid characters until the desired length password is reached.
 
 ### Password is displayed
 Once the desired password length is reached, it is either displayed in an alert, or is written to the page (screen). 
 
 ### Author
 Thomas van Deusen
+
--- script.js
+++ script.js
@@ -1,79 +1,60 @@
-// .Assignment Code
-var generateBtn = document.querySelector("#generate");
+//global vars for password generator
+var passwordLength;
+var approvedCharacters;
 
-//log value of generateBtn variable to console
-console.log(generateBtn);
+function generatePassword() {
+    //creating and returning password
+    getPasswordLength();
+    setOptions();
+    
+    var password = "";
 
-//create variables that indicate which types of characters user wants included
-var yesLowercase = prompt("Include lowercase letters in the password? Cancel or OK");
-var yesUppercase = prompt("Include uppercase letters in the password? Cancel or OK");
-var yesNumbers = prompt("Include numeric characters in the password? Cancel or OK");
-var yesSpecial = prompt("Include special characters in the password? Cancel or OK");
-
-//log variables to console
-console.log(yesLowercase);
-console.log(yesUppercase);
-console.log(yesNumbers);
-console.log(yesSpecial);
-
-//create array of replies
-var included = [yesLowercase, yesUppercase, yesNumbers, yesSpecial];
-  
-//log included array to console
-console.log(included);
-
-// Display prompt and assign response to var passwordHowLong
-var passwordHowLong = prompt("Your password must be at least 8 characters long, but not more than 128. What length password would you like?")
-console.log(passwordHowLong);
-
-// check user input to see if password length is valid
-if ( Number(passwordHowLong) <= 7 || (Number(passwordHowLong)) >128 ) {
-  alert("Please type a number between 8 and 128 for password length.");
+    for (let i = 0; i < passwordLength; i++) {
+        password += approvedCharacters[Math.floor(Math.random() * approvedCharacters.length)];
+    }
+    //display generated password in console
+    console.log(password);
+    //display generated password in document 
+    document.getElementById('passwordDisplay').innerHTML = password;
 }
 
-// check to see that at least one of the four character types is desired
-if (included[0] === null && included[1] === null && included[2] === null && included[3] === null) {
-  alert("Try again. You need to choose one or more character types for your password.");
-
+function getPasswordLength() {
+    passwordLength = prompt("Please enter a number between 8 and 128");
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("Password is too long or too short, please try again");
+        getPasswordLength();
+    } else {
+        console.log(passwordLength);
+    }
 }
 
-// set yesLowercase to true for testing regardless of user choice
-//yesLowercase = true;
+function setOptions() {
+    //set options
 
-//proof of concept when (at least) lowercase letters are requested
-// if(yesLowercase) {}
+    approvedCharacters = "";
+    if (confirm("Would you like to include lowercase?")) {
+        approvedCharacters += "qwertyuiopasdfghjklzxcvbn";
+    }
 
+    if (confirm("Would you like to include uppercase?")) {
+        approvedCharacters += "QWERTYUIOPASDFGHJKLZXCVBNM";
+    }
 
-// .Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+    if (confirm("Would you like to include numbers?")) {
+        approvedCharacters += "0123456789";
+    }
 
-};
+    if (confirm("Would you like to include any of these !@#$%^ special characters?")) {
+        approvedCharacters += "!@#$%^";
+    }
 
-// create a function to randomly return a character between 
-// lowercase a and lowercase z (inclusive).
-
-//Math.random creates a random number between 0 and 1
-//(Math.floor(Math.random()) * 26)) multiplies the 
-//random number by 26 and rounds down to nearest integer.
-//string.fromCharCode (references the browser character table
-//where "a" is code 97). The rounded-down number between 0 and 26 is
-//multiplied by 26 and added to 97 to return a value from a to z
-//according to the browser character table.
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-};
-
-//display in the console the random lowercase letter 
-console.log(getRandomLower);
-
-for (var i = 0; i < passwordHowLong.length; i++) {
-  getRandomLower();
-  console.log(i); 
+    //if the user did not pick any approved characters, alert, then call same function
+    if (!approvedCharacters) {
+        alert("Please choose at least one category of characters.");
+        //introduction to recursion;
+        setOptions();
+    } else {
+        console.log(approvedCharacters);
+    }
+    
 }
-
-// .Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
